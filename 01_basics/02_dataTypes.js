a852aa0cd4f341d50dba004f78ffce44291456c8
MohdFazil45 <mdfazil846@gmail.com>
2025-03-19 16:50:42 +0530

    Added new file 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+//alert(3 + 3) //we are using nodejs, not browser
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Fazil");
+
+let name = "Fazil"
+let age = "20"
+
+//number => 2 to power 53
+//bigInt 
+//string => ""
+//boolean => true/false
+//null => standalone
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // type aata hai undefined 
+console.log(typeof null);// type aata hai object
