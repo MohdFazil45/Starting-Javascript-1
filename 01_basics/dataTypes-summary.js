11b91b23d878991f1247f097d961d3c50413acea
MohdFazil45 <mdfazil846@gmail.com>
2025-03-20 16:41:40 +0530

    Updated memory topic dataTypes-summary.js

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -11,7 +11,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 const bigNumber = 982437582735872483n
 
@@ -31,4 +31,29 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello World");
 }
-console.log(typeof heros);
+// console.log(typeof heros);
+
+//*************************************************/
+
+//Stack (Primitive), Heap (Non_Primitive)
+
+
+let myYoutube = "Daredevil"
+let anothername = myYoutube
+anothername = "chai or code"
+
+console.log(myYoutube);
+console.log(anothername);
+
+let userOne = {
+    email: "chai@gmail.com",
+    upi: "chai@axis.io",
+}
+
+let userTwo = userOne
+
+userTwo.email = "code@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
