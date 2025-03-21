a64e67f724f4f5255d1dd6b7f4049cb2dacf8779
MohdFazil45 <mdfazil846@gmail.com>
2025-03-21 10:55:59 +0530

    Added string file 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name = "Fazil-op-007"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`hello my name id ${name} and my repo count ${repoCount}`)
+
+const gameName = new String('God Of War')   
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.charAt(9));
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('d'));
+const newString = gameName.substring(2,7);
+console.log(newString);
+const anotherString = gameName.slice(0,4);
+console.log(anotherString);
+
+const newStringOne = "   fazil   "
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+const url = "http://:fazil.com/api%20/original"
+
+
+console.log(url.replace('%20','-'));
+
+console.log(name.split('-'));
+
+
+
+
+
+
+
