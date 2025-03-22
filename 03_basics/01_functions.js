4aa866e6008b92c3d1b6c7c40896a8015debf13b
MohdFazil45 <mdfazil846@gmail.com>
2025-03-22 17:58:43 +0530

    Update in 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,4 +27,29 @@ function loginUserMessage(username){
     else return`${username} just logged in`
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+/**************************************/
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500))
+// 
+const user = {
+    username:"john",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+handleObject({
+    username:"jane",
+    price:299
+})
+const myNewArray =[200,400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 500, 1000]))
