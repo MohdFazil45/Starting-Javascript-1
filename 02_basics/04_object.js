7a3ce154f844c3e7ccfaa4cad1958efb2c74e1d7
MohdFazil45 <mdfazil846@gmail.com>
2025-03-22 16:07:17 +0530

    Added 04_objects.js file

---
+++ 02_basics/04_object.js
@@ -0,0 +1,61 @@
+//singleton
+
+const tinderUser  = new Object();
+const tinderUser2 = {}
+tinderUser.id = "123abc"
+tinderUser.name = "John"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"John@example.com",
+    fullname: {
+        userfullname:{
+            firstname:"John",
+            lastname:"Doe"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj2 = {
+    3:"c",
+    4:"d",  
+}
+
+// const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({},obj1,obj2) // mdn documentation
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const users = [
+    {
+    id: 1,
+    name: "John Doe",
+    email: "john@example.com"
+    },
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@example.com"
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+        email: "jane@example.com"
+    }
+]
+
+users[1].email = "jane@example.com";
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
