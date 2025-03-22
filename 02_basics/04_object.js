b67a498065daf39cdb74e5f2c06cfb8bd60d7236
MohdFazil45 <mdfazil846@gmail.com>
2025-03-22 16:23:26 +0530

    Updated structuring of object in 04_object.js file

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -28,34 +28,47 @@ const obj2 = {
 
 // const obj3 = { obj1, obj2}
 // const obj3 = Object.assign({},obj1,obj2) // mdn documentation
-const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
 
-const users = [
-    {
-    id: 1,
-    name: "John Doe",
-    email: "john@example.com"
-    },
-    {
-        id: 1,
-        name: "John Doe",
-        email: "john@example.com"
-    },
-    {
-        id: 2,
-        name: "Jane Doe",
-        email: "jane@example.com"
-    }
-]
+// const users = [
+//     {
+//     id: 1,
+//     name: "John Doe",
+//     email: "john@example.com"
+//     },
+//     {
+//         id: 1,
+//         name: "John Doe",
+//         email: "john@example.com"
+//     },
+//     {
+//         id: 2,
+//         name: "Jane Doe",
+//         email: "jane@example.com"
+//     }
+// ]
 
-users[1].email = "jane@example.com";
+// users[1].email = "jane@example.com";
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+
+//*************************  Destructuring  ******************************/
+
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstrustor:"Hitesh"
+}
+// course.coursename in this way we repeeatedly select data again again
+
+const {courseInstrustor: instructor} = course
+// console.log(courseInstrustor);
+console.log(instructor);
 
