cbe3b37806ff301306f483a4dc4e00fa5caf9c77
MohdFazil45 <mdfazil846@gmail.com>
2025-03-26 16:53:41 +0530

    Added forEach file

---
+++ 05_iteration/five.js
@@ -0,0 +1,38 @@
+const coding = ["js", "python", "java", "cpp", "ruby" ]
+
+// coding.forEach( (item) => {
+//     console.log(item)
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName, 'denoted by ', item.languageFileName );
+})
