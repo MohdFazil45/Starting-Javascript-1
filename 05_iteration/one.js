7482cbdfc285f2ad97568ef24c1864d38feeb1dc
MohdFazil45 <mdfazil846@gmail.com>
2025-03-26 15:25:33 +0530

    Added for loop file

---
+++ 05_iteration/one.js
@@ -0,0 +1,39 @@
+//for 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+for (let i = 0; i <= 10; i++) {
+    const element = i
+    if (i == 7) console.log("Thala for a reason");
+    //console.log(i)   
+}
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        //console.log(i + '*' + j + '=' + i*j);
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    //console.log(element);
+    
+}
+
+//break and continue
+
+for (let i = 0; i <= 20; i++) {
+    if (i == 5){
+        // console.log("5 Detected");
+        break;
+    }
+    // console.log(`Value of i is ${i}`);    
+}
+
+// While
