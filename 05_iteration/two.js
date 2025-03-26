71df538469703ae63b6674f98b887b801c6ab0dd
MohdFazil45 <mdfazil846@gmail.com>
2025-03-26 15:37:29 +0530

    Added while or do while file

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+// while (condition) {
+    
+// }
+
+// let index = 0 
+// while (index <= 10){
+//     //console.log(`Value of index ${index}`);
+//     index = index + 1
+// }
+
+let myArray =  ["flash", "batman", "superman"]
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+let score = 1
+do {
+    console.log(`Score :- ${score}`);
+    score++;
+    
+} while (score <= 10);
