53ff58fa57861aaf4ba8f2a8db86a03ba4e5b7e1
MohdFazil45 <mdfazil846@gmail.com>
2025-03-26 10:13:52 +0530

    Added  Control flow file one.js

---
+++ 04_control_fllow/one.js
@@ -0,0 +1,40 @@
+// if (condition){
+//     // code to run if condition is true
+// }
+
+// const isUserLoggedIn = 1
+//  if(isUserLoggedIn === true) {
+//     console.log('User is LoggedIn')
+//  }
+//  else console.log('User is  LoggedOut');
+ 
+// const temp = 40
+
+// if (temp <= 20){
+//    console.log('Winter');
+// }
+// else{console.log('Summer');
+// }
+
+// const balance = 1000
+
+// if (balance <= 500) console.log('test'), console.log('test2');
+
+
+// if (balance <500) {
+//    console.log("less than 500");
+// } else if (balance < 750) {
+//    console.log("less than 750");
+// } else if (balance < 900) {
+//    console.log("less than 1000");
+// } else {
+//    console.log("more than 900");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard) {
+   console.log("Allow to shop");
+   
+}
---
+++ 04_control_fllow/switch.js
@@ -0,0 +1,67 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 7
+
+// switch (month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log('february');
+//         break;
+//     case 3:
+//         console.log('march');
+//         break;
+//     default:
+//         console.log('none');
+//         break;
+// }
+
+// const userEmail = "faz@gmail.com";
+//  if (userEmail) {
+//     console.log("Got user email");
+//  } else {
+//     console.log("No user email found");
+//  }
+
+// falsy value
+
+//false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value
+
+//"0", 'false', " ", [], {}, function{}
+
+// if(userEmail.length === 0) {
+//     console.log("Array is empty"); 
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 =  undefined ?? 15
+// val1 = null ?? undefined
+
+// console.log(val1);
+
+// Ternary operator
+
+// condition ? true : false;
+ const iceTeaPrice =100
+ iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+ 
