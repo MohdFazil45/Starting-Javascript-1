eaf4fd9c0d0bbb76cc01be7f33da68f5b6f82ebd
MohdFazil45 <mdfazil846@gmail.com>
2025-04-19 23:46:35 +0530

    Added classes and inheritance file

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+    createId(){
+        return `123`
+    }
+}
+
+const fazil = new User("Fazil")
+console.log(fazil.createId());
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "Iphone@gmail.com")
+// iphone.logMe()
+console.log(iphone.createId());
